d21854e6369d9d82a5b9199f79644a19ad32507c
adityarouniyar99 <adityarouniyar99@gmail.com>
2026-03-23 23:16:59 +0530

    comparison of datatypes

---
+++ 04_Comparison_of_datatypes.js
@@ -0,0 +1,22 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console. log(2 == 1);
+// console. log(2 != 1);
+
+
+//console.log("2" > 1);
+//console.log("02" > 1); // different datatype comparison is illegal.
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);  // avoid this type of comparison.
+
+// console.log(undefined == 0);   // avoid this type of comparison.
+
+// comparison and equality check is different meanings.
+
+// strict check {===} in this datatype and values are check.
+
+console.log("2" === 2);
+
